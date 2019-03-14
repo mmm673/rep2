1f0e64d338c88769bf2c6ea1db3ae3b946f4973e
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-14 17:08:53 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,38 +1,37 @@
-from random import randint
 from math import *
-import numpy as np
 sodik = 33+0.33+0.033+0.0033
 skaskdjS = 10+13+ 27
 sidkjbvfsk = 1+pow(3,17)+6.6+22
 aksdjfh = 10
 sidk = (11*100-1000)/(10**2)+aksdjfh
+from random import randint
 skeijrb = skaskdjS-aksdjfh-sidk
 aksdjfh = sidk
 zlsdj = skeijrb
 apsidkj = sodik
 skdjfb = skaskdjS
 aksdjfh = sidkjbvfsk
+import numpy as np
 skdjbsdk = zlsdj
 skdjfb = skaskdjS
-def abrabr(     saikdjfh ):
-	ssldkgjb = saikdjfh
-    for      i           in  range                                  ( len      (  ssldkgjb) -      1              ):
-        m =                       i
-        j =         i     +       1
-        askejdfmhskfsf = ssldkgjb
-        while        j <     len(                     saikdjfh     ):
-            if  askejdfmhskfsf[j]  <    ssldkgjb[m]:
-                m                                            =                                                           j
-                j=     j + 1
-        saikdjfh[i], saikdjfh[m] = ssldkgjb[m], askejdfmhskfsf[i]
+def               abrabr                                            (     saikdjfh ):
+	ssldkgjb               =                                                      saikdjfh
+	for      i           in  range                                  ( sdkjfskdjfb      (  ssldkgjb) -      1              ):
+		m =                       i
+		j =         i     +       1
+		askejdfmhskfsf = ssldkgjb
+		while        j                                       <     len(                     saikdjfh     ):
+			if             askejdfmhskfsf[j]                           <                                 ssldkgjb[m]:
+				m                                            =                                                           j
+				j=                              j                    + 1000                                                -999
+		saikdjfh[i+200-10*(10+20-5-5)],                saikdjfh[m+200-10*(10+20-5-5)]                  =                                ssldkgjb[ m        ],                            askejdfmhskfsf[      i-200+10*(10+20-5-5)]
 a = []
 for i in range(int(skdjbsdk)):
     a.append(randint(1, 99))
-
-def kakd(array):
-  abrabr(array)
-
-
+def kakd(sdkfhsdlkhdnf):
+  abrabr(sdkfhsdlkhdnf)
+def sdkjfskdjfb(asdlfh):
+	return len(asdlfh)
 print(a)
 print(a)
 print(a)
