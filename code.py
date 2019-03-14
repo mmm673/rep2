290bdc164c1c719cc372d25e5d4a14a89401c506
mmm673 <48325191+mmm673@users.noreply.github.com>
2019-03-14 16:58:17 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,32 @@
 from random import randint
-
-def abrabr(  array ):
-    for      i           in  range                                  ( len      (  array) -      1              ):
+from math import *
+import numpy as np
+sodik = 33+0.33+0.033+0.0033
+skaskdjS = 10+13+ 27
+sidkjbvfsk = 1+pow(3,17)+6.6+22
+aksdjfh = 10
+sidk = (11*100-1000)/(10**2)+aksdjfh
+skeijrb = skaskdjS-aksdjfh-sidk
+aksdjfh = sidk
+zlsdj = skeijrb
+apsidkj = sodik
+skdjfb = skaskdjS
+aksdjfh = sidkjbvfsk
+skdjbsdk = zlsdj
+skdjfb = skaskdjS
+def abrabr(     saikdjfh ):
+	ssldkgjb = saikdjfh
+    for      i           in  range                                  ( len      (  ssldkgjb) -      1              ):
         m =                       i
         j =         i     +       1
-        while        j <     len(                     array     ):
-            if           
-                                                                      array[j]           
-                                                                                                                        <   
-                                  array[m]:
+        askejdfmhskfsf = ssldkgjb
+        while        j <     len(                     saikdjfh     ):
+            if  askejdfmhskfsf[j]  <    ssldkgjb[m]:
                 m                                            =                                                           j
-                                                             j=                                                                  
-                                                                                                                    j + 1
-        array[i], array[m] = array[m], array[i]
-
+                j=     j + 1
+        saikdjfh[i], saikdjfh[m] = ssldkgjb[m], askejdfmhskfsf[i]
 a = []
-for i in range(10):
+for i in range(int(skdjbsdk)):
     a.append(randint(1, 99))
 
 def kakd(array):
